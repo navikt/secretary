6dbea65f8625a6f4c50c62d7396e193a129c2441
Bjoern Frode Kvernstuen <bjorn.frode.kvernstuen@nav.no>
2019-03-13 16:33:33 +0100

    Added props

--- src/main/java/no/nav/hackathon/secretary/SecretaryApplication.java
+++ src/main/java/no/nav/hackathon/secretary/SecretaryApplication.java
@@ -3,11 +3,16 @@ package no.nav.hackathon.secretary;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Properties;
+
 @SpringBootApplication
 public class SecretaryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecretaryApplication.class, args);
+
+		Properties properties = new Properties();
+		properties.put("SLACK_CLIENT_ID", System.getenv("SLACK_CLIENT_ID"));
+		properties.put("BOT_TOKEN", System.getenv("BOT_TOKEN"));
 	}
-// --I was here
 }
