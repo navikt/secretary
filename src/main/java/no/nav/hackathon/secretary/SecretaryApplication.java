352063485acb910d0c8ef2d8919080823fa505c7
Nilsen, Rene <rene.nilsen@nav.no>
2019-03-13 16:50:26 +0100

    jbot

--- src/main/java/no/nav/hackathon/secretary/SecretaryApplication.java
+++ src/main/java/no/nav/hackathon/secretary/SecretaryApplication.java
@@ -2,12 +2,25 @@ package no.nav.hackathon.secretary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Properties;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"no.nav.hackathon.secretary.jbot", "no.nav.hackathon.secretary.slack"})
 public class SecretaryApplication {
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	/**
+	 * Entry point of the application. Run this method to start the sample bots,
+	 * but don't forget to add the correct tokens in application.properties file.
+	 *
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SecretaryApplication.class, args);
 
--- src/test/java/no/nav/hackathon/secretary/SecretaryApplicationTests.java
+++ src/test/java/no/nav/hackathon/secretary/SecretaryApplicationTests.java
@@ -1,16 +1,33 @@
 package no.nav.hackathon.secretary;
 
+import no.nav.hackathon.secretary.jbot.common.EventType;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SecretaryApplicationTests {
 
 	@Test
+	@Ignore
 	public void contextLoads() {
 	}
 
+	/**
+	 * Number of events in {@link EventType} should be
+	 * equal to the number of events supported by slack
+	 * (including 3 added by JBot) plus number of events
+	 * supported in fb bot.
+	 */
+	@Test
+    @Ignore
+	public void numberOfEventsType() {
+		EventType[] events = EventType.values();
+		assertEquals(87, events.length);
+	}
 }
